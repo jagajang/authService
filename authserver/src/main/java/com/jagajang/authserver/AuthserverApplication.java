6250dd6a3c1e3c7864861aebe6c0c4e3512dd9ce
jagajang <kosb7544@gmail.com>
2022-12-03 18:09:19 +0900

    dev: create auth server

---
+++ authserver/src/main/java/com/jagajang/authserver/AuthserverApplication.java
@@ -0,0 +1,13 @@
+package com.jagajang.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthserverApplication.class, args);
+    }
+
+}
