aee592d2c4fdcf668c0b73dc191c91dbe5c9d9f4
jagajang <kosb7544@gmail.com>
2022-12-03 18:08:05 +0900

    dev: create db server

---
+++ dbserver/src/test/java/com/jagajang/dbserver/DbserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jagajang.dbserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
