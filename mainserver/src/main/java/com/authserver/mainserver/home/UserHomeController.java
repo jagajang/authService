a943d0522e6926bd5e6893853d1ee57fc677fd85
jagajang <kosb7544@gmail.com>
2022-12-03 15:51:46 +0900

    dev: send model data from home controller to home view

--- mainserver/src/main/java/com/authserver/mainserver/home/UserHomeController.java
+++ mainserver/src/main/java/com/authserver/mainserver/home/UserHomeController.java
@@ -1,12 +1,15 @@
 package com.authserver.mainserver.home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserHomeController {
     @RequestMapping("/")
-    public String home() {
-        return "userpage/home";
+    public String home(Model model) {
+        model.addAttribute("role", "GUEST");
+
+        return "home";
     }
 }
