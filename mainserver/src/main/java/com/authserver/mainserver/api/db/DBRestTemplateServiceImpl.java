143c98b02141433fe3af976f7c6789759c30938d
jagajang <kosb7544@gmail.com>
2022-12-04 15:59:22 +0900

    main-refactor: change db api class name

--- mainserver/src/main/java/com/authserver/mainserver/api/db/DBRestTemplateServiceImpl.java
+++ mainserver/src/main/java/com/authserver/mainserver/api/db/DBRestTemplateServiceImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class MySQLRestTemplateService implements DBRestTemplateService {
+public class DBRestTemplateServiceImpl implements DBRestTemplateService {
     private final String domain = "localhost:8081";
 
     @Override
