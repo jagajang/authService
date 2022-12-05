ac98bb779bd6c96b732c961e1c1b9af3d81707fe
jagajang <kosb7544@gmail.com>
2022-12-05 19:49:51 +0900

    main-fix: remove role from form

--- mainserver/src/main/java/com/authserver/mainserver/auth/dto/UserRegisterForm.java
+++ mainserver/src/main/java/com/authserver/mainserver/auth/dto/UserRegisterForm.java
@@ -1,20 +1,16 @@
 package com.authserver.mainserver.auth.dto;
 
-import com.authserver.mainserver.type.UserRole;
 import lombok.Getter;
 
 @Getter
 public class UserRegisterForm extends UserInfoForm {
-    private UserRole role;
     private String nickname;
 
     public UserRegisterForm(
             String email,
             String password,
-            UserRole role,
             String nickname) {
         super(email, password);
-        this.role = role;
         this.nickname = nickname;
     }
 }
