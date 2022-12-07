56fdfea46dbec0b76355f06a965e08525bf81911
jagajang <kosb7544@gmail.com>
2022-12-07 15:30:43 +0900

    db-fix: remove db test code and seperate controller into guest&user and admin

---
+++ dbserver/src/main/java/com/jagajang/dbserver/AdminController.java
@@ -0,0 +1,4 @@
+package com.jagajang.dbserver;
+
+public class AdminController {
+}
---
+++ dbserver/src/main/java/com/jagajang/dbserver/GuestUserController.java
@@ -0,0 +1,18 @@
+package com.jagajang.dbserver;
+
+import com.jagajang.dbserver.type.dto.UserInfoName;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+public class GuestUserController {
+    @Autowired
+    private DBService dbService;
+
+    @PostMapping("/register")
+    public Boolean registerUser(UserInfoName registerInfo) {
+        return dbService.registerUser(registerInfo);
+    }
+}
