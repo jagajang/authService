66106113bdbdc1aaf3a3199cf9d2d7656d523b29
jagajang <kosb7544@gmail.com>
2022-12-07 15:49:27 +0900

    main-modify: change url mapping for test

--- mainserver/src/main/java/com/authserver/mainserver/api/ApiTestController.java
+++ mainserver/src/main/java/com/authserver/mainserver/api/ApiTestController.java
@@ -4,15 +4,17 @@ import com.authserver.mainserver.api.db.DBWebClientServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequestMapping("/test")
 public class ApiTestController {
     @Autowired
     DBWebClientServiceImpl dbWebClientService;
 
-    @GetMapping("/api-test")
+    @GetMapping("/db-api")
     public String apiTest() {
         return dbWebClientService.apiTest();
     }
