13846cd3b92393c8b133976a11bef8e03c82e22c
jagajang <kosb7544@gmail.com>
2022-12-07 19:30:15 +0900

    db-dev: add user select mapping

--- dbserver/src/main/java/com/jagajang/dbserver/Controller/GuestUserController.java
+++ dbserver/src/main/java/com/jagajang/dbserver/Controller/GuestUserController.java
@@ -2,9 +2,12 @@ package com.jagajang.dbserver.Controller;
 
 import com.jagajang.dbserver.DBService;
 import com.jagajang.dbserver.type.dto.UserMailPassName;
+import com.jagajang.dbserver.type.dto.UserResponseInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -15,5 +18,10 @@ public class GuestUserController {
     @PostMapping("/register")
     public Boolean registerUser(UserMailPassName registerInfo) {
         return dbService.registerUser(registerInfo);
+    }
+
+    @PostMapping("/search-all")
+    public List<UserResponseInfo> searchUsers(String nickname) {
+        return dbService.findUsersByNickname(nickname);
     }
 }
