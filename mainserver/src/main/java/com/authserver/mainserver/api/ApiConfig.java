428ce70a4a9488d488f5629981b354ff0a5bfaac
jagajang <kosb7544@gmail.com>
2022-12-08 12:08:40 +0900

    main-dev: seperate webclient into another bean and apply DI

---
+++ mainserver/src/main/java/com/authserver/mainserver/api/ApiConfig.java
@@ -0,0 +1,52 @@
+package com.authserver.mainserver.api;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class ApiConfig {
+    @Bean
+    public WebClient authWebClient() {
+        String domain = "http://localhost:8081";
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClientBuilder()))
+                .baseUrl(domain)
+                .defaultCookie("key", "value")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+
+    @Bean
+    public WebClient dbWebClient() {
+        String domain = "http://localhost:8082";
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClientBuilder()))
+                .baseUrl(domain)
+                .defaultCookie("key", "value")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+
+    private HttpClient httpClientBuilder() {
+        return HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 2000)
+                .responseTimeout(Duration.ofMillis(2000))
+                .doOnConnected(conn ->
+                        conn.addHandlerLast(new ReadTimeoutHandler(2000, TimeUnit.MILLISECONDS))
+                                .addHandlerLast(new WriteTimeoutHandler(2000, TimeUnit.MILLISECONDS))
+                );
+    }
+}
--- mainserver/src/main/java/com/authserver/mainserver/api/db/DBWebClientServiceImpl.java
+++ mainserver/src/main/java/com/authserver/mainserver/api/db/DBWebClientServiceImpl.java
@@ -5,6 +5,7 @@ import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
@@ -19,26 +20,8 @@ import java.util.concurrent.TimeUnit;
 @Service
 @Slf4j
 public class DBWebClientServiceImpl implements DBWebClientService {
-    private final WebClient dbWebClient;
-
-    DBWebClientServiceImpl() {
-        String domain = "http://localhost:8082";
-
-        HttpClient httpClient = HttpClient.create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 2000)
-                .responseTimeout(Duration.ofMillis(2000))
-                .doOnConnected(conn ->
-                    conn.addHandlerLast(new ReadTimeoutHandler(2000, TimeUnit.MILLISECONDS))
-                        .addHandlerLast(new WriteTimeoutHandler(2000, TimeUnit.MILLISECONDS))
-                );
-
-        dbWebClient = WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .baseUrl(domain)
-                .defaultCookie("key", "value")
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .build();
-    }
+    @Qualifier("dbWebClient")
+    private WebClient dbWebClient;
 
     @Override
     public String apiTest() {
